4e1164e184bc0b0eccae3ba8a1e3853e1e97c2b8
AI-Amponsah <Yawamp27@gmail.com>
2023-10-25 13:13:49 +0000

    pep

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,50 +1,63 @@
 #!/usr/bin/python3
-def is_winner(x, nums):
-    def sieve_of_eratosthenes(limit):
-        primes = [True] * (limit + 1)
-        primes[0] = primes[1] = False
+""" Prime Game """
 
-        for i in range(2, int(limit**0.5) + 1):
-            if primes[i]:
-                for j in range(i * i, limit + 1, i):
-                    primes[j] = False
 
-        return [num for num, is_prime in enumerate(primes) if is_prime]
+def isprime(n):
+    """ n: number to check if it is prime"""
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
 
-    def play_game(n):
-        primes = sieve_of_eratosthenes(n)
-        remaining_numbers = set(range(1, n + 1))
 
-        while remaining_numbers:
-            move = None
-            for prime in primes:
-                if prime in remaining_numbers:
-                    move = prime
+def delete_numbers(n, nums):
+    """ delete numbers - assign zero """
+    for i in range(len(nums)):
+        if nums[i] % n == 0:
+            nums[i] = 0
+
+
+def isWinner(x, nums):
+    """ where x is the number of rounds and nums is an array of n
+        Return: name of the player that won the most rounds
+        Iriaf the winner cannot be determined, return None
+        You can assume n and x will not be larger than 10000
+    """
+    nums.sort()
+    winner = False
+    Maria = 0
+    Ben = 0
+    for game in range(x):
+        # print("game# ", game+1)
+        nums2 = list(range(1, nums[game] + 1))
+        # print("nums: ", nums2)
+        turn = 0
+        while True:
+            """
+            # uncomment to monitor turns
+            if turn % 2 != 0:
+                print("Ben turn ")
+            else:
+                print("Maria turn ")
+            """
+            change = False
+            for i, n in enumerate(nums2):
+                # print("n: ", n, "i: ", i)
+                if n > 1 and isprime(n):
+                    delete_numbers(n, nums2)
+                    change = True
+                    turn += 1
                     break
-
-            if move is None:
+            # print("movement: ", nums2)
+            if change is False:
                 break
-
-            remaining_numbers -= set(range(move, n + 1, move))
-
-        return "Maria" if len(remaining_numbers) % 2 == 1 else "Ben"
-
-    wins = {"Maria": 0, "Ben": 0}
-
-    for n in nums:
-        winner = play_game(n)
-        if winner:
-            wins[winner] += 1
-
-    if wins["Maria"] > wins["Ben"]:
-        return "Maria"
-    elif wins["Maria"] < wins["Ben"]:
-        return "Ben"
-    else:
+        if turn % 2 != 0:
+            Maria += 1
+        else:
+            Ben += 1
+        # print("Maria: {}, Ben: {}".format(Maria, Ben))
+    if Maria == Ben:
         return None
-
-# Example usage:
-x = 3
-nums = [4, 5, 1]
-result = is_winner(x, nums)
-print(result)
+    if Maria > Ben:
+        return "Maria"
+    return "Ben"
