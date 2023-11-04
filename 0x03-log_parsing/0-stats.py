ab71cd37bf268d94021606af3309e0e5aa294acb
AI-Amponsah <Yawamp27@gmail.com>
2023-11-04 19:02:54 +0000

    sdin

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,38 +1,43 @@
 #!/usr/bin/python3
 '''Write a script that reads stdin line by line and then computes metrics'''
 
-
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
+def compute_metrics():
+    cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+             '403': 0, '404': 0, '405': 0, '500': 0}
+    total_size = 0
+    counter = 0
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+    try:
+        for line in sys.stdin:
+            line_list = line.split(" ")
+            if len(line_list) > 4:
+                code = line_list[-2]
+                size = int(line_list[-1])
+                if code in cache:
+                    cache[code] += 1
+                total_size += size
+                counter += 1
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+            if counter == 10:
+                counter = 0
+                print('File size: {}'.format(total_size))
+                for key, value in sorted(cache.items()):
+                    if value != 0:
+                        print('{}: {}'.format(key, value))
 
-except Exception as err:
-    pass
+    except ValueError as ve:
+        sys.stderr.write(f"Error: {ve}\n")
+    except Exception as ex:
+        sys.stderr.write(f"Unexpected Error: {ex}\n")
 
-finally:
-    print('File size: {}'.format(total_size))
+    return total_size, cache
+
+if __name__ == "__main__":
+    total_size, cache = compute_metrics()
+
+    print('Final File size: {}'.format(total_size))
     for key, value in sorted(cache.items()):
         if value != 0:
             print('{}: {}'.format(key, value))
-            
